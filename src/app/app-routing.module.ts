9a6e12be4753edfdf5255446f78eea689c56e0fd
Eito Takase <eito01178@gmail.com>
2022-11-20 16:11:09 +0900

    実行ページのルートコンポーネントを作成

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'play',
+    loadChildren: () =>
+      import('./routed/play/play.module').then((m) => m.PlayModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
